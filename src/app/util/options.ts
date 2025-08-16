59923c84aff5423bc41603dbbd29fba61c391ed3
Narthana Sathsara <narthanasathsara93@gmail.com>
2025-08-16 20:03:01 +0900

    sadasd

--- src/app/util/options.ts
+++ src/app/util/options.ts
@@ -1,12 +1,13 @@
 export const options = [
   { label: 'ධර්ම දාන', value: 'dharma_dana' },
   { label: 'සාංඝික දාන', value: 'sanghika_dana' },
-  { label: 'භාණ්ඩ/උපකරණ පූජා ', value: 'upakarana_puja' },
   { label: 'විහාර කර්මාන්ත', value: 'vihara_karmanta' },
-  { label: 'ආලෝක පූජා', value: 'aloka_pooja' },
   { label: 'අභය දාන ', value: 'abahay_dana' },
   { label: 'වස්සාන කාල සහ කඨිණ පින්කම්', value: 'kathina' },
+  { label: 'විශේෂ පූජා', value: 'wishesha_puja' },
+  { label: 'ආලෝක පූජා', value: 'aloka_pooja' },
   { label: 'තෙරුවන්ට පූජා', value: 'theruwanta_pooja' },
+  { label: 'භාණ්ඩ/උපකරණ පූජා ', value: 'upakarana_puja' },
   { label: 'ස්වර්ණමාලී මහා සෑය', value: 'swarnamali_maha_seya' },
   { label: 'පිරිකර පූජා', value: 'pirikara_pooja' },
   { label: 'ශිලා සමාදාන', value: 'shila_samadana' },
@@ -14,16 +15,16 @@ export const options = [
   { label: 'අධ්‍යාපන', value: 'adhyaapana' },
   { label: 'තිරිසන් සතුන් සම්බන්ධ', value: 'thirisan_sathun_sambanda' },
   { label: 'සෞඛ්‍ය සම්බන්ධ', value: 'saukhya_sambandha' },
+  { label: 'වන රෝපණ', value: 'wana_ropana' },
   { label: 'විවිධ', value: 'vividha' },
 ];
-
 
 export const statusOptions = [
   { label: 'සම්පූර්ණයි', value: 'DONE' },
   { label: 'භාර ගත්', value: 'PROMISED' },
   { label: 'අදහස් කර ගත්', value: 'TO_BE_DONE' },
   { label: 'අත්හිටවූ ', value: 'ABANDONED' },
-]
+];
 
 export interface Option {
   label: string;
